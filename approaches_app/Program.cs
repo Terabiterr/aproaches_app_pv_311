41df6c4eb2b815f2ed9f9401f5902f496ac10d09
test <test@gmail.com>
2024-11-07 21:05:15 +0200

    add service

--- approaches_app/Program.cs
+++ approaches_app/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Linq;
+﻿using approaches_app.Services;
+using System;
 
 namespace approaches_app
 {
@@ -7,56 +7,16 @@ namespace approaches_app
     {
         static void Main(string[] args)
         {
-            var product = new Product
+            using (var db = new ServiceProduct(new ShopEntities()))
             {
-                Name = "TV",
-                Price = 15000.00m
-            };
-            //Create(product);
-            Read();
-        }
-        public static void Read()
-        {
-            //Read
-            using (var db = new ShopEntities())
+                db.CreateProduct(new Product { Name = "Xbox360", Price = 700.00m });
+            }
+            using (var db = new ServiceProduct(new ShopEntities()))
             {
-                var products = db.Products.ToList();
+                var products = db.GetProducts();
                 foreach (var p in products)
                 {
-                    Console.WriteLine($"Id: {p.id}, Name: {p.Name}, Price: {p.Price}");
-                }
-            }
-        }
-        public static void Create(Product product)
-        {
-            using (var db = new ShopEntities())
-            {
-                db.Products.Add(product);
-                db.SaveChanges();
-            }
-        }
-        public static void Update(int id, Product product)
-        {
-            using (var db = new ShopEntities())
-            {
-                var product_to_update = db.Products.Find(id);
-                if (product_to_update != null)
-                {
-                    product_to_update.Name = product.Name;
-                    product_to_update.Price = product.Price;
-                    db.SaveChanges();
-                }
-            }
-        }
-        public static void Delete(int id)
-        {
-            using (var db = new ShopEntities())
-            {
-                var product = db.Products.Find(id);
-                if(product != null)
-                {
-                    db.Products.Remove(product);
-                    db.SaveChanges();
+                    Console.WriteLine(p.Name);
                 }
             }
         }
---
+++ approaches_app/Services/ServiceProduct.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace approaches_app.Services
+{
+    public interface IServiceProduct
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductById(int id);
+        void CreateProduct(Product product);
+        void UpdateProduct(int id, Product product);
+        void DeleteProduct(int id);
+    }
+    public class ServiceProduct : IServiceProduct, IDisposable
+    {
+        private readonly ShopEntities _db;
+        public ServiceProduct(ShopEntities db)
+        {
+            _db = db;
+        }
+        public void CreateProduct(Product product)
+        {
+            _db.Products.Add(product);
+            _db.SaveChanges();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var product = _db.Products.Find(id);
+            if (product != null)
+            {
+                _db.Products.Remove(product);
+                _db.SaveChanges();
+            }
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public Product GetProductById(int id)
+            => _db.Products.Find(id);
+
+        public IEnumerable<Product> GetProducts()
+            => _db.Products.ToList();
+
+        public void UpdateProduct(int id, Product product)
+        {
+            var product_to_update = _db.Products.Find(id);
+            if (product_to_update != null)
+            {
+                product_to_update.Name = product.Name;
+                product_to_update.Price = product.Price;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
